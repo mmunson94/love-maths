4be2c2fd1dc2e200446eb26e14ddcc34076ca23a
mmunson94 <michaeljackmunson9994@outlook.com>
2023-01-02 00:50:00 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,31 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else { 
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gametype}`);
+                runGame(gameType);
             }
         })
-    }
+    } 
+
+    runGame("addition")
+
 })
 
-function runGame() {
+/** 
+ * the main game "loop", called when the script is first loaded
+ * and after the user's script has been answered
+*/
+function runGame(gameType) {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditonQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -40,7 +57,11 @@ function displayAdditonQuestion() {
 
 }
 
-function displayAdditonQuestion() {
+function displayAdditonQuestion(operand1, operand2) {
+    
+    document.getElementById(operand1).textContent = operand1;
+    document.getElementById(operand2).textContent = operand2;
+    document.getElementById(operator).textContent = "+";
 
 }
 
